41cb939f389f82dae894aac314c9e506e7b6405e
JGibson2019 <jacqueline.gibson@utexas.edu>
2020-06-20 11:36:31 -0700

    about changes addition

---
+++ personal/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\jagibson\\repos\\jgibson2019.github.io\\personal\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\jagibson\\repos\\jgibson2019.github.io\\personal\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\jagibson\\repos\\jgibson2019.github.io\\personal\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\jagibson\\repos\\jgibson2019.github.io\\personal\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
+  require("C:\\Users\\jagibson\\repos\\jgibson2019.github.io\\personal\\src\\css\\custom.css"),
+];
