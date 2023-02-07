c9d30dc5bab39c06d52a2f6412b8021af7f18715
JGibson2019 <jcg2216@columbia.edu>
2023-02-06 18:54:00 -0600

    adding document.js file to set html lang attribute

---
+++ nextjs-blog/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+// Using this file to set lang="en"
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
